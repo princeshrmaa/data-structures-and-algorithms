8b8fecb0ae77e0fae1d495ee2f093fb3448e20cc
princesharma216 <princesharma7247@gmail.com>
2026-08-02 19:55:58 +0530

    Solved Sum Of Digits

---
+++ src/LoopsAndMath/Sumofdigits.java
@@ -0,0 +1,18 @@
+package LoopsAndMath;
+
+import java.util.Scanner;
+
+public class Sumofdigits {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        int n=sc.nextInt();
+        int sum=0;
+        while(n!=0){
+            int dig=n%10;
+            sum=sum+dig;
+            n=n/10;
+        }
+        System.out.println(sum);
+        sc.close();
+    }
+}
