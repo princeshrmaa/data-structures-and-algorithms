46cb90f877ba457fc1080086694086e31cd7b089
princesharma216 <princesharma7247@gmail.com>
2026-08-02 20:05:48 +0530

    Solved Prime Number

---
+++ src/LoopsAndMath/PrimeNumber.java
@@ -0,0 +1,33 @@
+package LoopsAndMath;
+
+import java.util.Scanner;
+
+public class PrimeNumber {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+
+        int n = sc.nextInt();
+
+        if (n <= 1) {
+            System.out.println("Not Prime");
+            return;
+        }
+
+        boolean prime = true;
+
+        for (int i = 2; i < n; i++) {
+            if (n % i == 0) {
+                prime = false;
+                break;
+            }
+        }
+
+        if (prime) {
+            System.out.println("Prime");
+        } else {
+            System.out.println("Not Prime");
+        }
+
+        sc.close();
+    }
+}
