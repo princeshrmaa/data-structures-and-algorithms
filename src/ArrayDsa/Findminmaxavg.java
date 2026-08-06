11d2d72ab760fb5a98405f01b68874f2eca6146e
princesharma216 <princesharma7247@gmail.com>
2026-08-06 15:44:27 +0530

    Find the min,max and avg

---
+++ src/ArrayDsa/Findminmaxavg.java
@@ -0,0 +1,29 @@
+package ArrayDsa;
+
+import java.util.Scanner;
+
+public class Findminmaxavg {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        int n=sc.nextInt();
+        int arr[]=new int[n];
+        for(int i=0;i<n;i++){
+            arr[i]=sc.nextInt();
+        }
+        int max=arr[0];
+        int min=arr[0];
+        int sum=0;
+        for(int i=0;i<n;i++){
+            if(max<arr[i]){
+                max=arr[i];
+            }
+            if(min>arr[i]){
+                min=arr[i];
+            }
+            sum=sum+arr[i];
+        }
+        System.out.println("The maximum marks is :- "+max);
+        System.out.println("The minimum marks is :- "+min);
+        System.out.println("The Average marks is :- "+sum/n);
+    }
+}
---
+++ src/LoopsAndMath/Fibonacci.java
@@ -0,0 +1,7 @@
+package LoopsAndMath;
+
+public class Fibonacci {
+    static void main(String[] args) {
+        
+    }
+}
