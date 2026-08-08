9a28323c37904f6d39c188fa7127a6e91459477e
princesharma216 <princesharma7247@gmail.com>
2026-08-08 14:42:50 +0530

    Solved Q1 Find Maximum Element

---
+++ src/ArrayDsa/FindMaxEle.java
@@ -0,0 +1,21 @@
+package ArrayDsa;
+
+import java.util.Scanner;
+
+public class FindMaxEle {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        int n=sc.nextInt();
+        int arr[]=new int[n];
+        for(int i=0;i<arr.length;i++){
+            arr[i]=sc.nextInt();
+        }
+        int max=Integer.MIN_VALUE;
+        for(int i=0;i<arr.length;i++){
+            if(max<arr[i]){
+                max=arr[i];
+            }
+        }
+        System.out.println(max);
+    }
+}
