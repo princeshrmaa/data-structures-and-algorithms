578f6acd7902c03ccfdffc3d9f0154ac20df6e41
princesharma216 <princesharma7247@gmail.com>
2026-08-19 20:18:39 +0530

    Solve String Q8 - Reverse String

---
+++ src/Stringdsa/ReverseString.java
@@ -0,0 +1,19 @@
+package Stringdsa;
+
+import java.util.Scanner;
+
+public class ReverseString {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        String s = sc.nextLine();
+
+        String a = "";
+
+        for (int i = s.length() - 1; i >= 0; i--) {
+            a = a + s.charAt(i);
+        }
+
+        System.out.println(a);
+    }
+}
+
