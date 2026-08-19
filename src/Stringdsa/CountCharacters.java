7618fc29daf44d3d20f18f1113ca771770457e33
princesharma216 <princesharma7247@gmail.com>
2026-08-19 19:33:16 +0530

    Solve String Q2 - Count Characters

---
+++ src/Stringdsa/CountCharacters.java
@@ -0,0 +1,11 @@
+package Stringdsa;
+
+import java.util.Scanner;
+
+public class CountCharacters {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        String s = sc.nextLine();
+        System.out.println(s.length());
+    }
+}
