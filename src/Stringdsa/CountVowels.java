0ac233b1ca1034abd3c846fa62925ee07feec16f
princesharma216 <princesharma7247@gmail.com>
2026-08-19 19:39:05 +0530

    Solve String Q3 - Count Vowels

---
+++ src/Stringdsa/CountVowels.java
@@ -0,0 +1,18 @@
+package Stringdsa;
+
+import java.util.Scanner;
+
+public class CountVowels {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        String s = sc.nextLine();
+        s=s.toLowerCase();
+        int vowelcount=0;
+        for(int i=0;i<s.length();i++){
+            if(s.charAt(i)=='a'||s.charAt(i)=='e'||s.charAt(i)=='i'||s.charAt(i)=='o'||s.charAt(i)=='u'){
+                vowelcount++;
+            }
+        }
+        System.out.println(vowelcount);
+    }
+}
