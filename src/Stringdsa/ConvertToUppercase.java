475f8fa6a6fa6146f44433015bc4f9aed318cc6f
princesharma216 <princesharma7247@gmail.com>
2026-08-20 06:57:53 +0530

    Solve String Q17 - Convert to Uppercase

---
+++ src/Stringdsa/ConvertToUppercase.java
@@ -0,0 +1,12 @@
+package Stringdsa;
+
+import java.util.Scanner;
+
+public class ConvertToUppercase {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        String s = sc.nextLine();
+        s=s.toUpperCase();
+        System.out.println(s);
+    }
+}
