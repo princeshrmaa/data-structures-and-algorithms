469ead7cf683f61157f62d0d0cef78d2c009e164
princesharma216 <princesharma7247@gmail.com>
2026-08-20 06:48:10 +0530

    Solve String Q14 - Check Substring

---
+++ src/Stringdsa/CheckSubstring.java
@@ -0,0 +1,12 @@
+package Stringdsa;
+
+import java.util.Scanner;
+
+public class CheckSubstring {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        String s= sc.nextLine();
+        String target = sc.nextLine();
+        System.out.println(s.contains(target));
+    }
+}
