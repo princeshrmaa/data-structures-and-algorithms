ef0ad2e2eac5db04597a25d36dde2112906956b3
princesharma216 <princesharma7247@gmail.com>
2026-08-20 06:59:53 +0530

    Solve String Q18 - Convert to Lowercase

---
+++ src/Stringdsa/ConvertToLowercase.java
@@ -0,0 +1,11 @@
+package Stringdsa;
+
+import java.util.Scanner;
+
+public class ConvertToLowercase {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        String s = sc.nextLine();
+        System.out.println(s.toLowerCase());
+    }
+}
